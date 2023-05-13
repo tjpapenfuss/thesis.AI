7afcf24e8185cf680fe181a6f63575c3a4a3e895
Tanner Papenfuss <tjpapenfuss@gmail.com>
2023-05-13 11:00:42 -0400

    added print statements. removed unnecessary code

--- web_scrape/testing.py
+++ web_scrape/testing.py
@@ -1,19 +1,12 @@
 import requests
-
-# URL = "https://aws.amazon.com/solutions/case-studies/caremonitor/"
-# page = requests.get(URL)
-
-# print(page.text)
-
-import requests
 from bs4 import BeautifulSoup
 
 def extract_text_from_url(url):
     try:
-        # Send a request to the specified URL
+        # Send a request to the specified URL. 
         response = requests.get(url)
 
-        # If the request succeeded, move further
+        # If the request succeeded, move further. If the request fails, print the error code. 
         if response.status_code == 200:
             # Parse the obtained HTML content using BeautifulSoup
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -27,16 +20,17 @@ def extract_text_from_url(url):
                 #"-----------------------------------------\n")
                 my_string = my_string + (element.get_text(strip=True)) + "\n"
 
+            # Not using any of the below, except return statment. 
 
             # Remove script and style elements (as they won't have any useful text)
-            for script_style_element in soup(['script', 'style']):
-                script_style_element.extract()
+            # for script_style_element in soup(['script', 'style']):
+            #     script_style_element.extract()
 
-            raw_text = soup.get_text()
+            # raw_text = soup.get_text()
 
-            # Eliminate unwanted whitelines
-            lines = [x.strip() for x in raw_text.split("\n") if x.strip()]
-            final_text = "\n".join(lines)
+            # # Eliminate unwanted whitelines
+            # lines = [x.strip() for x in raw_text.split("\n") if x.strip()]
+            # final_text = "\n".join(lines)
             #print(my_string)
             #return final_text
             return my_string
@@ -59,3 +53,4 @@ if __name__ == "__main__":
                 out_file_name = ''.join(e for e in page_text[:8] if e.isalnum()) + ".txt"
                 with open(out_file_name, "w", encoding="utf-8") as output_file:
                     output_file.write(page_text)
+                print("Webpage " + url + "\nhas been successfully writen to file: " + out_file_name)
